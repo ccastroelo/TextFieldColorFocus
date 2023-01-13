e5906279b4aa2b4a6e453549466181b63420ed57
ccastroelo <claudiodecastro@gmail.com>
2023-01-13 15:17:13 -0300

    utilizando focus para gerencias a mudança de cor

--- lib/main.dart
+++ lib/main.dart
@@ -32,10 +32,17 @@ class _MyHomePageState extends State<MyHomePage> {
   int idxFocus = 0;
   TextEditingController controller1 = TextEditingController();
   TextEditingController controller2 = TextEditingController();
+  FocusNode focusBt = FocusNode();
+  bool onValidate = false;
 
   String? validator(String? value) {
     if (value?.isEmpty ?? true) return "preenchimento obrigatório";
     return null;
+  }
+
+  void onChange(String value) {
+    onValidate = false;
+    setState(() {});
   }
 
   @override
@@ -55,37 +62,28 @@ class _MyHomePageState extends State<MyHomePage> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               SelectTextField(
-                controller: controller1,
-                hint: "campo 1",
-                idx: 1,
-                idxFocus: idxFocus,
-                initColor: Colors.blue,
-                selectColor: Colors.amber,
-                prefixIcon: Icons.person,
-                onTap: (value) {
-                  setState(() {
-                    idxFocus = value;
-                  });
-                },
-              ),
+                  onChange: onChange,
+                  onValidate: onValidate,
+                  validate: validator,
+                  controller: controller1,
+                  hint: "campo 1",
+                  initColor: Colors.blue,
+                  selectColor: Colors.amber,
+                  prefixIcon: Icons.person,
+                  focusChange: () => setState(() {})),
               const SizedBox(
                 height: 20,
               ),
               SelectTextField(
-                invalidMsg: "campo obrigatório",
-                controller: controller2,
-                hint: "campo 2",
-                idx: 2,
-                idxFocus: idxFocus,
-                initColor: Colors.blue,
-                selectColor: Colors.amber,
-                prefixIcon: Icons.person,
-                onTap: (value) {
-                  setState(() {
-                    idxFocus = value;
-                  });
-                },
-              ),
+                  onChange: onChange,
+                  onValidate: onValidate,
+                  validate: validator,
+                  controller: controller2,
+                  hint: "campo 2",
+                  initColor: Colors.blue,
+                  selectColor: Colors.amber,
+                  prefixIcon: Icons.person,
+                  focusChange: () => setState(() {})),
               const SizedBox(
                 height: 20,
               ),
@@ -93,8 +91,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 height: 20,
               ),
               ElevatedButton(
+                focusNode: focusBt,
                 onPressed: () {
-                  // Validate returns true if the form is valid, or false otherwise.
+                  focusBt.requestFocus();
+                  onValidate = true;
+                  setState(() {});
 
                   print(controller1.text);
                   print(controller2.text);
@@ -112,93 +113,99 @@ class SelectTextField extends StatefulWidget {
     super.key,
     this.hint,
     this.hintColor = Colors.white54,
-    required this.idx,
-    required this.idxFocus,
     required this.initColor,
     required this.selectColor,
     required this.prefixIcon,
     this.textColor = Colors.white,
-    required this.onTap,
+    required this.focusChange,
     required this.controller,
-    this.invalidMsg,
+    required this.validate,
+    required this.onValidate,
+    required this.onChange,
   });
   final String? hint;
   final Color? hintColor;
-  final int idx;
-  final int idxFocus;
   final Color initColor;
   final Color selectColor;
   final IconData prefixIcon;
   final Color? textColor;
-  final ValueSetter onTap;
+  final VoidCallback focusChange;
   final TextEditingController controller;
-  final String? invalidMsg;
+  final String? Function(String?) validate;
+  final bool onValidate;
+  final void Function(String) onChange;
 
   @override
   State<SelectTextField> createState() => _SelectTextFieldState();
 }
 
 class _SelectTextFieldState extends State<SelectTextField> {
-  String? validatorMsg;
+  String? validateMsg;
+  bool onFocus = false;
 
   @override
   void didUpdateWidget(SelectTextField oldWidget) {
     super.didUpdateWidget(oldWidget);
-    if (oldWidget.idxFocus != widget.idxFocus ||
-        oldWidget.invalidMsg != widget.invalidMsg) {
-      setState(() {});
+
+    if (widget.onValidate) {
+      validateMsg = widget.validate(widget.controller.text);
     }
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        AnimatedContainer(
-          duration: const Duration(milliseconds: 100),
-          decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                    color: Colors.grey[400]!,
-                    offset: Offset.fromDirection(1.2, 2),
-                    blurRadius: 2,
-                    spreadRadius: 1)
-              ],
-              borderRadius: const BorderRadius.all(Radius.circular(10)),
-              color: widget.invalidMsg != null
-                  ? Colors.red
-                  : widget.idx == widget.idxFocus
-                      ? widget.selectColor
-                      : widget.initColor),
-          child: TextField(
-            controller: widget.controller,
-            textAlignVertical: TextAlignVertical.center,
-            onTap: () {
-              widget.onTap(widget.idx);
-            },
-            style: TextStyle(color: widget.textColor),
-            cursorColor: widget.hintColor,
-            decoration: InputDecoration(
-                prefixIcon: Icon(
-                  widget.prefixIcon,
-                  color: widget.idx == widget.idxFocus
-                      ? widget.textColor
-                      : widget.hintColor,
-                ),
-                hintText: widget.hint,
-                hintStyle: TextStyle(color: widget.hintColor),
-                border: InputBorder.none),
-          ),
-        ),
-        if (widget.invalidMsg?.isNotEmpty ?? false)
-          Align(
-            alignment: Alignment.center,
-            child: Text(
-              widget.invalidMsg!,
-              style: const TextStyle(color: Colors.red),
+    return Focus(
+      onFocusChange: (hasFocus) {
+        onFocus = hasFocus;
+        if (hasFocus) {
+          widget.focusChange();
+        }
+      },
+      child: Column(
+        children: [
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 100),
+            decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey[400]!,
+                      offset: Offset.fromDirection(1.2, 2),
+                      blurRadius: 2,
+                      spreadRadius: 1)
+                ],
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+                color: widget.onValidate && (validateMsg?.isNotEmpty ?? false)
+                    ? Colors.red
+                    : onFocus
+                        ? widget.selectColor
+                        : widget.initColor),
+            child: TextField(
+              onChanged: widget.onChange,
+              controller: widget.controller,
+              textAlignVertical: TextAlignVertical.center,
+              style: TextStyle(color: widget.textColor),
+              cursorColor: widget.hintColor,
+              decoration: InputDecoration(
+                  prefixIcon: Icon(
+                    widget.prefixIcon,
+                    color: onFocus ? widget.textColor : widget.hintColor,
+                  ),
+                  hintText: widget.hint,
+                  hintStyle: TextStyle(color: widget.hintColor),
+                  border: InputBorder.none),
             ),
-          )
-      ],
+          ),
+          if (widget.onValidate && (validateMsg?.isNotEmpty ?? false))
+            const Align(
+              alignment: Alignment.center,
+              child: Text(
+                "campo obrigatório",
+                style: TextStyle(color: Colors.red),
+              ),
+            )
+        ],
+      ),
     );
   }
 }
